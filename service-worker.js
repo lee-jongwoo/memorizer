533fc03b6a57caa5a47922f0142317eb8d1e1ffa
Jongwoo Lee <jongwoo@jongwoo.dev>
2025-03-16 14:09:42 +0900

    Improve caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,16 @@
-const CACHE_NAME = 'v0.1';
+const CACHE_NAME = 'v0.2';
+
+var passage_paths = [];
+const response = await fetch('texts/index.json');
+const passages = await response.json();
+passages.forEach(passage => {
+  passage_paths.push('texts' + passage.file);
+});
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
       caches.open(CACHE_NAME).then((cache) => {
-          return cache.addAll(['/', '/index.html', '/styles/style.css', '/scripts/app.js', 'texts/']);
+          return cache.addAll(['/', '/index.html', '/styles/style.css', '/scripts/app.js'].concat(passage_paths));
       })
   );
 });
